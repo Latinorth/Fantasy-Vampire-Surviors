fe28fccb58d611fba36371d6c58751fc2a049041
Latinorth <jackhoward525@gmail.com>
2024-07-05 23:52:56 -0500

    asda

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,13 +6,13 @@ using UnityEngine.UIElements;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private Rigidbody2D rb;
+    public Rigidbody2D Playerrb;
     private Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        Playerrb = GetComponent<Rigidbody2D>();
     }
 
     // FixedUpdate is called at a fixed interval and is used for physics calculations
@@ -23,6 +23,6 @@ public class Movement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         // Move the player
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Playerrb.MovePosition(Playerrb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
